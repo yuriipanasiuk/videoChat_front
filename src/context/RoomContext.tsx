14180eb82553c79d52651b291f723a48bff681e8
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-03-20 22:32:43 +0200

    add video player

--- src/context/RoomContext.tsx
+++ src/context/RoomContext.tsx
@@ -1,6 +1,10 @@
-import React, { createContext, useEffect } from "react";
+import React, { createContext, useEffect, useState, useReducer } from "react";
 import { useNavigate } from "react-router-dom";
 import socketIo from "socket.io-client";
+import Peer from "peerjs";
+import { v4 } from "uuid";
+import { peerReducer } from "./peerReducer";
+import { addPeerAction } from "./peerActions";
 
 const HOST_BACKEND = "http://localhost:8080";
 
@@ -12,18 +16,60 @@ interface RoomProviderProps {
 }
 
 export const RoomProvider: React.FC<RoomProviderProps> = ({ children }) => {
+  const [me, setMe] = useState<Peer>();
+  const [stream, setStream] = useState<MediaStream>();
+  const [peers, dispatch] = useReducer(peerReducer, {});
+
   const navigate = useNavigate();
 
   const enterRoom = ({ roomId }: { roomId: string }): any => {
     navigate(`/room/${roomId}`);
   };
 
+  const getUsers = ({ participants }: { participants: string[] }) => {
+    console.log({ participants });
+  };
+
   useEffect(() => {
+    const meId = v4();
+    const peer = new Peer(meId);
+    setMe(peer);
+
+    try {
+      navigator.mediaDevices
+        .getUserMedia({ video: false, audio: true })
+        .then((stream) => {
+          setStream(stream);
+        });
+    } catch (error) {
+      console.error(error);
+    }
     webSocket.on("room-created", enterRoom);
+    webSocket.on("get-users", getUsers);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    if (!me || !stream) return;
+
+    webSocket.on("user-joined", ({ peerId }) => {
+      const call = me.call(peerId, stream);
+      call.on("stream", (peerStream) => {
+        dispatch(addPeerAction(peerId, peerStream));
+        console.log(peerStream);
+      });
+    });
+
+    me.on("call", (call) => {
+      call.answer(stream);
+      call.on("stream", (peerStream) => {
+        dispatch(addPeerAction(call.peer, peerStream));
+      });
+    });
+  }, [me, stream]);
+
   return (
-    <RoomContext.Provider value={{ webSocket }}>
+    <RoomContext.Provider value={{ webSocket, me, stream, peers }}>
       {children}
     </RoomContext.Provider>
   );
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,18 +1,24 @@
 import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { VideoPlayer } from "../components/VideoPlayer/VideoPlayer";
+import { PeerState } from "../context/peerReducer";
 import { RoomContext } from "../context/RoomContext";
 
 export const Room = () => {
   const { id } = useParams();
-  const { webSocket } = useContext(RoomContext);
+  const { webSocket, me, stream, peers } = useContext(RoomContext);
 
   useEffect(() => {
-    webSocket.emit("join-room", { roomId: id });
-  }, [id, webSocket]);
+    if (me) webSocket.emit("join-room", { roomId: id, peerId: me._id });
+  }, [id, me, webSocket]);
 
   return (
     <>
       <p>room {id}</p>
+      <VideoPlayer stream={stream} />
+      {Object.values(peers as PeerState).map((peer) => {
+        return <VideoPlayer stream={peer.stream} />;
+      })}
     </>
   );
 };
